58c09dfe851ffcd092a159dd3b2b4621f61a389e
DIOMANDE Manan Yves Lionel <diomandemananyveslionel@MacBook-Pro-de-DIOMANDE.local>
2024-07-03 23:46:55 +0000

    correct database communication error

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,6 +1,11 @@
 from pymongo import MongoClient # type: ignore
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-client = MongoClient("mongodb://localhost:27017/")
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+
 db = client.whatsapp_db
 
 def get_db():
