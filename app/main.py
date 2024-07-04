799e1fc3ec058a4bc54bdd94c798ddb9bc6154fc
DIOMANDE Manan Yves Lionel <diomandemananyveslionel@MacBook-Pro-de-DIOMANDE.local>
2024-07-04 19:07:05 +0000

    setup grafana && promootheus

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 from app.auth.routes import router as auth_router
 from app.chats.routes import router as chats_router
 from app.stories.routes import router as stories_router
@@ -7,13 +8,15 @@ from app.upload.routes import router as upload_router
 
 app = FastAPI()
 
+# Add Prometheus middleware
+Instrumentator().instrument(app).expose(app)
+
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(chats_router, prefix="/chats", tags=["chats"])
 app.include_router(stories_router, prefix="/stories", tags=["stories"])
 app.include_router(calls_router, prefix="/calls", tags=["calls"])
 app.include_router(upload_router, prefix="/upload", tags=["upload"])
 
-
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the whataspp API"}
+    return {"message": "Welcome to the whatsapp API"}
