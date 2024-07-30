ed9d0f29631d5922b9a58262f34577696a4611b4
DIOMANDE Manan Yves Lionel <diomandemananyveslionel@MacBook-Pro-de-DIOMANDE.local>
2024-07-30 20:04:51 +0000

    latest version

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL,echo=True)
+engine = create_engine(DATABASE_URL)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
